cb3223172dd4bee20b7b769d88448d99543f4175
Laura Kishimoto <laura@inviqa.com>
2019-02-01 22:56:35 +0000

    Updates

---
+++ precache-manifest.6cd7973f96fad10a05105072d64bedde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41c5389202168540ff8f",
+    "url": "/laurakishimoto/static/js/runtime~main.41c53892.js"
+  },
+  {
+    "revision": "59fe21b7261e24f2882f",
+    "url": "/laurakishimoto/static/js/main.59fe21b7.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/laurakishimoto/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "59fe21b7261e24f2882f",
+    "url": "/laurakishimoto/static/css/main.f41a169e.chunk.css"
+  },
+  {
+    "revision": "f701566e5c4141d3b54ecdcbf3a1d37e",
+    "url": "/laurakishimoto/index.html"
+  }
+];
