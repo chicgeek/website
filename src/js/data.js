145bb46384fa9155decc077d0160792d5b5a342e
Laura Kishimoto <laura@inviqa.com>
2019-02-04 09:46:07 +0000

    Update content

--- src/js/data.js
+++ src/js/data.js
@@ -6,8 +6,8 @@ export const data = [
     title: <Fragment>Personal <span>details</span></Fragment>,
     content: <Fragment>
       <h3>About me</h3>
-      <p>I'm a Canadian living and working in Sheffield&nbsp;UK.</p>
       <p>I am a front end developer with over 10 years of experience as part of agency and in-house teams. In addition to technical excellence, I am a leader of people and advocate for inclusive work cultures.</p>
+      <p>I'm a Canadian living and working in Sheffield&nbsp;UK. I have a master's degree in music and love design, board games, and red pandas.</p>
     </Fragment>
   },
   {
@@ -17,19 +17,17 @@ export const data = [
       <h3>User-centric approach</h3>
       <p>I focus on the intersection of user need, client demand, and technical limitation. I prioritise user experience, accessibility, and brand identity in my work.</p>
       <h3>HTML, CSS, JavaScript</h3>
-      <p>I write accessible HTML styled with SCSS. I have experience with React, Redux, Styled Components, and complex JavaScript applications. I work with my team to ensure quality code, modularity, and maintainability.</p>
+      <p>I write accessible HTML styled with SCSS. My JavaScript experience includes React, Redux, and Styled Components. I work with my team to ensure quality code, modularity, and maintainability.</p>
    </Fragment>
   },
   {
     id: 'leadership',
     title: <Fragment><span>People</span> leadership</Fragment>,
     content: <Fragment>
-      <h3>Leader and organiser</h3>
-      <p>I am organised and respected by my coworkers. I strive to create and encourage a safe and inclusive work environment for everyone.</p>
-      <h3>Manager and mentor</h3>
-      <p>I am an empathetic and supportive line manager. I work hard to enable others to do their best and I play an active role in recruitment.</p>
-      <h3>Community contributor</h3>
-      <p>Outside of work, I am well-connected to the Sheffield tech community. I am also an organiser of <a href="https://twitter.com/frontendsheff">Front End Sheffield</a>, as well as a previous organiser of Sheffield JS.</p>
+      <h3>Leader and mentor</h3>
+      <p>I am a reliable and trusted member of any team. I earn the respect of my coworkers through leadership and enabling others to do their best. I am an empathetic and supportive manager and enjoy mentoring other developers.</p>
+      <h3>Culture and community</h3>
+      <p>I strive to create and encourage a safe and inclusive work environment for everyone. Outside of work, <a href="https://inviqa.com/blog/how-user-groups-enrich-our-tech-community">I am involved with user groups</a>. I organise <a href="https://twitter.com/frontendsheff">Front&nbsp;End Sheffield</a> and previously organised Sheffield&nbsp;JS.</p>
     </Fragment>
   },
   {
@@ -38,19 +36,23 @@ export const data = [
     content: <Fragment>
       <ul>
         <li>
-          <h3>Inviqa</h3>
+          <h3>
+            <a href="https://inviqa.com/">Inviqa</a>
+          </h3>
           Technical team lead, Senior front end developer (2013&nbsp;&#8209;&nbsp;present)
         </li>
         <li>
-          <h3>Evoluted</h3>
+          <h3>
+            <a href="https://www.evoluted.net/">Evoluted</a>
+          </h3>
           Lead front end developer (2017&nbsp;&#8209;&nbsp;2018)
         </li>
         <li>
           <h3>Previous roles</h3>
-          Mountain Equipment Co-op,
-          Vancity,
-          University of York,
-          University of Western Ontario
+          <a href="https://www.mec.ca/">Mountain&nbsp;Equipment&nbsp;Co-op</a>,{' '}
+          <a href="https://www.vancity.com/">Vancity</a>,{' '}
+          <a href="https://www.york.ac.uk/">University&nbsp;of&nbsp;York</a>,{' '}
+          <a href="https://www.uwo.ca/">University&nbsp;of&nbsp;Western&nbsp;Ontario</a>
         </li>
       </ul>
     </Fragment>
