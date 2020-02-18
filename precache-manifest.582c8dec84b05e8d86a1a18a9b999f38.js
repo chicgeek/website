5a841feb18b7ffbf39b8a6c5f5438f6f7ea53927
Laura Kishimoto <laura@inviqa.com>
2020-02-18 11:06:52 -0500

    Updates

---
+++ precache-manifest.582c8dec84b05e8d86a1a18a9b999f38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0842bf234c02e8da44891f7813d3bb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98e282990b551dedf42c",
+    "url": "/static/css/main.2c6fd200.chunk.css"
+  },
+  {
+    "revision": "c9483b159c34c1f3f844",
+    "url": "/static/js/2.45c72ff3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.45c72ff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98e282990b551dedf42c",
+    "url": "/static/js/main.36658d10.chunk.js"
+  },
+  {
+    "revision": "8ae9d556d691fc2f1074",
+    "url": "/static/js/runtime-main.acf739d0.js"
+  }
+]);
